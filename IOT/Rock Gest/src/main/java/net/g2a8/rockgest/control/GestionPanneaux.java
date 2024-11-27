c6da8a4f0a64f35d3150e20b3023773e28194eea
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2024-11-27 16:50:15 +0100

    Restart fxml

---
+++ IOT/Rock Gest/src/main/java/net/g2a8/rockgest/control/GestionPanneaux.java
@@ -0,0 +1,47 @@
+package net.g2a8.rockgest.control;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import net.g2a8.rockgest.view.GestionPanneauxViewController;
+
+public class GestionPanneaux {
+    private Stage panneauStage;
+    private GestionPanneauxViewController panneauxViewController;
+    
+    
+
+
+    public GestionPanneaux(Stage _parentStage) {
+
+		try {
+			FXMLLoader loader = new FXMLLoader(
+			GestionPanneauxViewController.class.getResource("PanneauAffichage.fxml"));
+			BorderPane root = loader.load();
+
+			Scene scene = new Scene(root, root.getPrefWidth() + 50, root.getPrefHeight() + 10);
+
+			this.panneauStage = new Stage();
+			this.panneauStage.initModality(Modality.NONE);
+			this.panneauStage.initOwner(_parentStage);
+			this.panneauStage.setScene(scene);
+			this.panneauStage.setTitle("Gestion Panneaux Solaire");
+            this.panneauStage.setMaximized(true);
+			this.panneauStage.setResizable(true);
+
+			this.panneauxViewController = loader.getController();
+			this.panneauxViewController.initContext(this.panneauStage, this);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	    
+    void doPanneauxDialog(){
+		this.panneauxViewController.showDialog();;
+    }
+
+}
