95a31365ffc972202e00e43928a7d85540bce907
Matthias LIONARDO <137904345+mtthIA@users.noreply.github.com>
2024-11-28 16:43:56 +0100

    Update RockGest.java

--- IOT/JavaFx/src/main/java/application/RockGest.java
+++ IOT/JavaFx/src/main/java/application/RockGest.java
@@ -6,8 +6,6 @@ import application.loader.DataLoader;
 public class RockGest {
     public static void main(String[] args) {
         RockGestionMain.runApp();
-        DataLoader dataLoader = new DataLoader();
-        dataLoader.LoadDatasFromJson("dataNormal.json");
-        dataLoader.affichagerData();
+
     }
 }
