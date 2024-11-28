8837f410993b51eb5d5f75faf75d630c14e3a784
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-11-28 10:41:59 +0100

    recupérer les valeurs json(pas fini)

---
+++ IOT/JavaFx/src/main/java/application/loader/DataValue.java
@@ -0,0 +1,22 @@
+package application.loader;
+
+public class DataValue {
+    private String date;
+    private double value;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
