bd2e77542555673d3161411fe5d0dcbf0fb591a5
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-12-06 12:25:38 +0100

    moe

--- IOT/Systeme/enumType.py
+++ IOT/Systeme/enumType.py
@@ -1,8 +1,18 @@
 from enum import Enum
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+config.read(config_path)
+
+# Configuration
+nomNormal = config.get('configTopic', 'nomFichierDonnees')
+nomStrange = config.get('configTopic', 'nomFichierDonneesStrange')
 
 class DataType(Enum):
-    NORMAL = "dataNormal.json"
-    STARNGE = "dataStrange.json"
+    NORMAL = nomNormal + ".json"
+    STARNGE = nomStrange + ".json"
     
 class ValueTypeCap(Enum):
     CO2 = "co2"
