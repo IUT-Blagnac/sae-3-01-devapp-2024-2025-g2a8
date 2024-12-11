43dbd821260b6f73ad440b5876b52ee4b63f0a33
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-12-11 16:26:35 +0100

    fin affichage categorie

--- Site/include/menu.php
+++ Site/include/menu.php
@@ -18,28 +18,27 @@
                 foreach($categ->fetchAll(PDO::FETCH_ASSOC) as $cat){ ?>
                     <li class="nav-item display-navBar">
                         <a class="nav-link" href="#.php"><?php echo $cat['nom_categorie'] ?></a>
-                        <div class="navBarToDisplay">
-                            <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
-                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
-                                <div class="collapse navbar-collapse" id="navbarNav">
-                                    <ul class="navbar-nav">
-                                        <?php
-                                            $sousCateg = $conn->prepare("SELECT * FROM Categorie WHERE parent = :cat");
-                                            $sousCateg->execute(array(['cat' => $cat['id_categorie']]));
-
-                                            foreach($sousCateg->fetchAll(PDO::FETCH_ASSOC) as $sousCat){ ?>
+                        
+                        <?php
+                            $sousCateg = $conn->prepare("SELECT * FROM Categorie WHERE parent = :cat");
+                            $sousCateg->execute(['cat' => $cat['id_categorie']]);
+                            if($sousCateg->rowCount() > 0){ ?>
+                                <div class="navBarToDisplay">
+                                    <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
+                                    
+                                        <div class="collapse navbar-collapse" id="navbarNav">
+                                            <ul class="navbar-nav">
+                                                <?php foreach($sousCateg->fetchAll(PDO::FETCH_ASSOC) as $sousCat){ ?>
                                                 <li class="nav-item">
-                                                    <a class="nav-link" href="#.php"><?php echo $sousCat['nom_categorie'] ?></a>
+                                                    <a class="nav-link" href="sousCategorie.php?idSousCateg=<?php echo $sousCat['id_categorie']; ?>"><?php echo $sousCat['nom_categorie']; ?></a>                                                
                                                 </li>
                                             <?php } $sousCateg -> closeCursor();?>
-                                    </ul>
+                                            </ul>
+                                        </div>
+                                    </nav>
                                 </div>
-                            </nav>
-                        </div>
                     </li>
-            <?php } ?>
+            <?php }} $categ->closeCursor()?>
         </ul>
     </div>
 </nav>
---
+++ Site/sousCategorie.php
@@ -0,0 +1,31 @@
+<?php
+require_once("./include/head.php");
+?>
+
+<body class="d-flex flex-column min-vh-100">
+    <!-- En-tête -->
+    <?php    
+        require_once("./include/header.php");
+    ?>
+
+    <!-- Conteneur principal -->
+    <div class="container-fluid flex-grow-1">
+        <div class="row">
+            <!-- Menu vertical sur la gauche -->
+            <?php
+                require_once("./include/menu.php");
+            ?>
+
+            <!-- Contenu principal -->
+            <main role="main" class="col-md-9 ms-sm-auto col-lg-10 px-4">
+
+            </main>
+        </div>
+    </div>
+
+    <!-- Pied de page -->
+    <?php
+    require_once("./include/footer.php")
+    ?>
+</body>
+</html>
