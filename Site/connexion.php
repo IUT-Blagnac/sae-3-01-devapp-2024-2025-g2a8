4af77b2be0b75a940b9f237c2920f07a678eb292
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2024-12-13 16:14:49 +0100

    center form connexion

--- Site/connexion.php
+++ Site/connexion.php
@@ -48,25 +48,28 @@ require_once("./include/head.php");
         }
         ?>
         <p onload="appendAlert('Mot de passe invalide', 'danger')"></p>
-        <div class="form-signin w-50">
-            <form method="post">
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="mailConnexion" name="mail" placeholder="E-Mail">
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="passwordConnexion" name="pass"
-                        placeholder="Mot de passe">
-                </div>
-                <div class="form-check text-start my-3">
-                    <input class="form-check-input" type="checkbox" value="remember-me" id="checkRemember"
-                        name="remember">
-                    <label class="form-check-label" for="checkRemember">
-                        Se souvenir de moi
-                    </label>
-                    <button class="btn btn-primary w-100 py-2" type="submit" name="valider">Sign in</button>
-                </div>
-            </form>
-        </div>
+        <center>
+            <div class="form-signin w-50">
+                <form method="post">
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="mailConnexion" name="mail" placeholder="E-Mail">
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="passwordConnexion" name="pass"
+                            placeholder="Mot de passe">
+                    </div>
+                    <div class="form-check text-start my-3">
+                        <input class="form-check-input" type="checkbox" value="remember-me" id="checkRemember"
+                            name="remember">
+                        <label class="form-check-label" for="checkRemember">
+                            Se souvenir de moi
+                        </label>
+                        <button class="btn btn-primary w-100 py-2" type="submit" name="valider">Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </center>
+
         <div id="liveAlertPlaceholder"></div>
     </div>
 
