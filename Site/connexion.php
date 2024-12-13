d25cf1b6e80b202bc4690ed6e9375cbd316b96d1
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2024-12-13 16:45:11 +0100

    Fix script 2

--- Site/connexion.php
+++ Site/connexion.php
@@ -19,6 +19,31 @@ require_once("./include/head.php");
     <!-- Conteneur principal -->
     <div class="container flex-grow-1 text-center w-50">
         <h1>Connexion</h1>
+        <center>
+            <div class="form-signin w-50 text-center">
+                <form method="post">
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="mailConnexion" name="mail" placeholder="E-Mail"
+                            required>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="passwordConnexion" name="pass"
+                            placeholder="Mot de passe" required>
+                    </div>
+                    <div class="form-floating text-start my-3">
+                        <input class="form-check-input" type="checkbox" value="remember-me" id="checkRemember"
+                            name="remember">
+                        <label class="form-check-label" for="checkRemember">
+                            Se souvenir de moi
+                        </label>
+                        <button class="btn btn-primary w-100 py-2" type="submit" name="valider">Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </center>
+
+        <div id="liveAlertPlaceholder"></div>
+
         <?php
         if (isset($_POST["valider"]) and isset($_POST['mail']) and isset($_POST['pass'])) {
             include("include/connect.inc.php");
@@ -49,30 +74,6 @@ require_once("./include/head.php");
             }
         }
         ?>
-        <center>
-            <div class="form-signin w-50 text-center">
-                <form method="post">
-                    <div class="form-floating">
-                        <input type="email" class="form-control" id="mailConnexion" name="mail" placeholder="E-Mail"
-                            required>
-                    </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" id="passwordConnexion" name="pass"
-                            placeholder="Mot de passe" required>
-                    </div>
-                    <div class="form-floating text-start my-3">
-                        <input class="form-check-input" type="checkbox" value="remember-me" id="checkRemember"
-                            name="remember">
-                        <label class="form-check-label" for="checkRemember">
-                            Se souvenir de moi
-                        </label>
-                        <button class="btn btn-primary w-100 py-2" type="submit" name="valider">Sign in</button>
-                    </div>
-                </form>
-            </div>
-        </center>
-
-        <div id="liveAlertPlaceholder"></div>
     </div>
 
     <!-- Pied de page -->
