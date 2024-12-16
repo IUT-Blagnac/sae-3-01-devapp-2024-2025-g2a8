2ebc7f81714acd5acbd7fa0153e3315bb12159c3
Matthias <matthias.lionardo@gmail.com>
2024-12-16 21:47:14 +0100

    fin

--- Site/compte.php
+++ Site/compte.php
@@ -3,10 +3,10 @@ require_once("./include/head.php");
 ?>
 
 <body class="d-flex flex-column min-vh-100">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <!-- En-tête -->
     <?php    
         require_once("./include/header.php");
+        require_once("./include/verifConnexion.php");
     ?>
 
     <!-- Conteneur principal -->
