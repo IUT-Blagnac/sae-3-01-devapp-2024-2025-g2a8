5da99fc07f113f7316dbc867bb0b5fff26f36245
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2025-01-07 17:58:15 +0100

    Quantiter panier commande

--- Site/commander.php
+++ Site/commander.php
@@ -50,7 +50,7 @@ require_once("./include/head.php");
                 <div class="col-md-4 order-md-2 mb-4">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-muted">Votre panier</span>
-                        <span class="badge badge-secondary badge-pill">3</span>
+                        <span class="badge badge-secondary badge-pill"><?php echo getItemCartNumber($_SESSION['user_id']) ?></span>
                     </h4>
                     <ul class="list-group mb-3">
                         <?php
