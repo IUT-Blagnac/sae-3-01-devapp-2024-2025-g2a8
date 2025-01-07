315e9ab208597088a60b73538c6cb488b3d2b07d
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2025-01-07 19:05:15 +0100

    Alert commande

--- Site/commander.php
+++ Site/commander.php
@@ -11,19 +11,15 @@ require_once("./include/head.php");
     ?>
 
     <!-- form respond -->
-    <?php
-    if (isset($_POST["commande"])) {
-        
-    }
-    ?>
 
     <?php
-        $userCommande = getUserById($_SESSION['user_id'])
+    $userCommande = getUserById($_SESSION['user_id'])
     ?>
 
     <!-- Conteneur principal -->
     <div class="container-fluid flex-grow-1">
         <div class="container">
+            <div id="liveAlertPlaceholder"></div>
             <div class="row">
                 <div class="col-md-4 order-md-2 mb-4">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
@@ -79,14 +75,16 @@ require_once("./include/head.php");
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label for="firstName">Nom</label>
-                                <input type="text" class="form-control" id="firstName" value="<?php echo $userCommande["nom"]?>" name="firstName" required>
+                                <input type="text" class="form-control" id="firstName"
+                                    value="<?php echo $userCommande["nom"] ?>" name="firstName" required>
                                 <div class="invalid-feedback">
                                     Valid first name is required.
                                 </div>
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="lastName">Prenom</label>
-                                <input type="text" class="form-control" id="lastName" value="<?php echo $userCommande["prenom"]?>" name="lastName" required>
+                                <input type="text" class="form-control" id="lastName"
+                                    value="<?php echo $userCommande["prenom"] ?>" name="lastName" required>
                                 <div class="invalid-feedback">
                                     Valid last name is required.
                                 </div>
@@ -95,7 +93,8 @@ require_once("./include/head.php");
 
                         <div class="mb-3">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" value="<?php echo $userCommande["mail"]?>" name="email">
+                            <input type="email" class="form-control" id="email"
+                                value="<?php echo $userCommande["mail"] ?>" name="email">
                             <div class="invalid-feedback">
                                 Mail invalide !
                             </div>
@@ -109,16 +108,16 @@ require_once("./include/head.php");
                             </div>
                             <div class="col-md-9 mb-3 mb-3">
                                 <label for="nomRue">Nom de rue</label>
-                                <input type="text" class="form-control" id="nomRue" placeholder="Rue du fort" name="nomRue"
-                                    required>
+                                <input type="text" class="form-control" id="nomRue" placeholder="Rue du fort"
+                                    name="nomRue" required>
                             </div>
                         </div>
 
                         <div class="mb-3">
                             <label for="compadresse">Complement d'adresse (Optionnel)</label>
                             <small class="text-muted">Num d'appartement, code interphone, ...</small>
-                            <input type="text" class="form-control" id="compadresse" placeholder="Appartement 18" name="compadresse"
-                                required>
+                            <input type="text" class="form-control" id="compadresse" placeholder="Appartement 18"
+                                name="compadresse" required>
                         </div>
 
                         <div class="row">
@@ -137,7 +136,8 @@ require_once("./include/head.php");
                             </div>
                             <div class="mb-3">
                                 <label for="ville">Ville</label>
-                                <input type="text" class="form-control" id="ville" placeholder="Barbaira" name="ville" required>
+                                <input type="text" class="form-control" id="ville" placeholder="Barbaira" name="ville"
+                                    required>
                             </div>
                         </div>
                         <hr class="mb-4">
@@ -147,7 +147,8 @@ require_once("./include/head.php");
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label for="cc-name">Nom du titulaire</label>
-                                <input type="text" class="form-control" id="cc-name" placeholder="" name="cc-name" required>
+                                <input type="text" class="form-control" id="cc-name" placeholder="" name="cc-name"
+                                    required>
                                 <small class="text-muted">Votre nom sur la carte</small>
                                 <div class="invalid-feedback">
                                     Nom du titulaire requis
@@ -155,7 +156,8 @@ require_once("./include/head.php");
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="cc-number">Numero de carte</label>
-                                <input type="text" class="form-control" id="cc-number" placeholder="" name="cc-number" required>
+                                <input type="text" class="form-control" id="cc-number" placeholder="" name="cc-number"
+                                    required>
                                 <small class="text-muted">Le numero au devant de la carte</small>
                                 <div class="invalid-feedback">
                                     Numero requis
@@ -165,7 +167,8 @@ require_once("./include/head.php");
                         <div class="row">
                             <div class="col-md-3 mb-3">
                                 <label for="cc-expiration">Expiration</label>
-                                <input type="text" class="form-control" id="cc-expiration" name="cc-expiration" required>
+                                <input type="text" class="form-control" id="cc-expiration" name="cc-expiration"
+                                    required>
                                 <small class="text-muted">Date d'expiration au format MM/AA</small>
                                 <div class="invalid-feedback">
                                     Expiration requise
@@ -181,11 +184,56 @@ require_once("./include/head.php");
                             </div>
                         </div>
                         <hr class="mb-4">
-                        <button class="btn btn-primary btn-lg btn-block" type="submit" name="commande">Commander</button>
+                        <button class="btn btn-primary btn-lg btn-block" type="submit"
+                            name="commande">Commander</button>
                     </form>
                 </div>
             </div>
         </div>
+        <?php
+        if (isset($_POST["commande"])) {
+
+            if(!isset($_POST["firstName"])){
+                echo "<script>appendAlert('Nom obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["lastName"])){
+                echo "<script>appendAlert('Prenom obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["email"])){
+                echo "<script>appendAlert('Mail obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["numRue"])){
+                echo "<script>appendAlert('Numero de rue obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["nomRue"])){
+                echo "<script>appendAlert('Nom de rue obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["pays"])){
+                echo "<script>appendAlert('Pays obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["cp"])){
+                echo "<script>appendAlert('Code postale obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["ville"])){
+                echo "<script>appendAlert('Ville obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["cc-name"])){
+                echo "<script>appendAlert('Titulaire de carte obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["cc-number"])){
+                echo "<script>appendAlert('Numero de carte obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["cc-expiration"])){
+                echo "<script>appendAlert('Date d'expiration de carte obligatoire !', 'danger')</script>";
+
+            } else if(!isset($_POST["cc-cvv"])){
+                echo "<script>appendAlert('Code de securité obligatoire !', 'danger')</script>";
+
+            } else {
+
+            }
+        }
+    ?>
         <!-- Pied de page -->
         <?php
         require_once("./include/footer.php")
